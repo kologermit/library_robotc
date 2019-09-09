0e905bc7058ff29a5ec625e2ffb4dbd1604ea7e7
Kologermanskiy Fedor <kologermit@gmail.com>
2019-09-09 16:42:55 +0400

    v 1.0.0

--- Start.c
+++ Start.c
@@ -1,5 +1,18 @@
 #include <library.h>
+bool isBanka = false;
+task Banka()
+{
+	while(true)
+		Start(D, !isBanka ? 100 : -100);
+}
 task main()
 {
-	Move(BC, 100, 100);
+	startTask(Banka);
+	while(true)
+	{
+		isBanka = false;
+		delay(1000);
+		isBanka = true;
+		delay(1000);
+	}
 }
