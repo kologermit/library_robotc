8aed8eb8509f67dc876e40f6027d75d22b146da8
Kologermanskiy Fedor <kologermit@gmail.com>
2019-09-11 16:09:49 +0400

    v1.1.5

--- Start.c
+++ Start.c
@@ -13,12 +13,12 @@ void Zahvat()
 		Move(BC, 500, -100);
 		Start(D, -100);
 		delay(500);
-		Move(BC, 800, 100);
+		Move(BC, 850, 100);
 		Start(D, 100);
 		delay(500);
 	}
 }
-const int speed = 50;
+const int speed = 100;
 bool isKalibrovka = true;
 task Kalibrovka()
 {
@@ -28,17 +28,21 @@ task Kalibrovka()
 		isLine(S4);
 	}
 }
-void LINE()
+void LINE(int k1 = 1, int k2 = 1)
 {
-	while(isLine(S4))
-		if(isLine(S2))
+	k1 = k1 != -1 ? 0 : -1;
+	k2 = k2 != -1 ? 0 : -1;
+	Start((k2 != -1 ? C : B), speed);
+	while(!isLine(S2));
+	while(isLine(S4) + k1)
+		if(isLine(S2) + k2)
 	{
 		Start(B, speed);
-		Start(C, 0);
+		Start(C, speed * 0.5);
 	}
 	else
 	{
-		Start(B, 0);
+		Start(B, speed * 0.5);
 		Start(C, speed);
 	}
 	Start(BC, 0);
@@ -110,6 +114,7 @@ task main()
 		Start(B, 0);
 		Start(C, speed);
 	}
+	while(true);
 	Start(BC, 0);
 	Start(D, -100);
 	delay(500);
@@ -137,6 +142,7 @@ task main()
 		Move(BC, 50, speed);
 	}
 	MoveToFunc({Start(B, speed); Start(C, -speed);}, 450);
+	while(true);
 	for(int i = 0; i < 3; i++)
 	{
 		LINE();
@@ -145,5 +151,24 @@ task main()
 		Start(BC, speed);
 		while(!isLine(S4));
 		Move(BC, 50, speed);
+	}
+	LINE();
+	Start(BC, -speed);
+	while(!isLine(S4));
+	Start(BC, 0);
+	MoveToFunc({Start(B, speed); Start(C, -speed);}, 460);
+	for(int i = 0; i < 3; i++){
+		while(isLine(S4))
+			if(!isLine(S2))
+		{
+			Start(B, speed);
+			Start(C, 0);
+		}
+		else
+		{
+			Start(B, 0);
+			Start(C, speed);
+		}
+		Move(BC, 400, speed);
 	}
 }
